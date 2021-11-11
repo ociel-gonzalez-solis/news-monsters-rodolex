50eff1c95f134623a97415ff20f431c8a85e5f80
ociel-gonzalez-solis <ocielgonzalezsolis@hotmail.com>
2021-11-11 15:43:40 -0800

    Agregando nueva version a github pages

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    // this.handleChange = this.handleChange.bind(this); //Necesaria para funciones diferentes a las tipo flecha
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange =(e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    // <CardList monsters={this.state.monsters} />;
+    // <input
+    //   type="search"
+    //   placeholder="Search monsters"
+    //   onChange={(e) => this.setState({ searchField: e.target.value })}
+    // />;
+    return (
+      <div className="App">
+        <h1>Monster Rolex 2</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
