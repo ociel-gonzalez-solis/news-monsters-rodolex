6d91852237732d5dd188f6f429f48d39e710a24d
ociel-gonzalez-solis <ocielgonzalezsolis@hotmail.com>
2021-11-11 16:50:42 -0800

    Nuevos archivos 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("http://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((users) => this.setState({ monsters: users }));
   }
